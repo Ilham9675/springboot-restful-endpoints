80816c08bfb41a03ce1f77e0fb0b60782dc6c6fd
Ilham9675 <ilhamakbar9675@gmail.com>
2024-08-15 19:58:16 +0700

    Initial commit

---
+++ src/main/java/restapi/springboot_restful_endpoints/SpringbootRestfulEndpointsApplication.java
@@ -0,0 +1,13 @@
+package restapi.springboot_restful_endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulEndpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restapi/springboot_restful_endpoints/SpringbootRestfulEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package restapi.springboot_restful_endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
